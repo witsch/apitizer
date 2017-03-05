bb697b5852b1119b8f0cf2e5b048aa3f3c2f12cb
Andreas Zeidler <az@zitc.de>
2017-03-05 17:02:30 +0100

    Replace `Products.CMFCore.getToolByName` with `plone.api.portal.get_tool`.

--- apitizer.py
+++ apitizer.py
@@ -1,7 +1,26 @@
 from rope.base.project import Project
+from rope.contrib import generate
+from rope.refactor import rename, move
+
+
+def get_function(project, module, name):
+    mod = project.get_module(module)
+    offset = mod.source_code.index('def {}('.format(name)) + len('def ')
+    return mod.resource, offset
 
 
 def main():
+    # set up project & fake `plone.api`
     project = Project('.')
-    print('refactor! :)')
+    generate.create_package(project, 'plone')
+    pkg = generate.create_package(project, 'plone.api')
+    portal = generate.create_module(project, 'portal', pkg)
+
+    # replace `getToolByName`
+    func = get_function(project, 'Products.CMFCore.utils', 'getToolByName')
+    project.do(move.create_move(project, *func).get_changes(portal))
+    func = get_function(project, 'plone.api.portal', 'getToolByName')
+    project.do(rename.Rename(project, *func).get_changes('get_tool'))
+
+    # clean up
     project.close()
